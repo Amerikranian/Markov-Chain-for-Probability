2ff3dfa322f03258071ae2b57df89a90883f4299
unknown <olegjpittman@gmail.com>
2023-04-08 18:20:14 -0500

    Adds callback for markov simulation

--- markov_chain.py
+++ markov_chain.py
@@ -25,16 +25,18 @@ class MarkovChain:
     def _check_chain_properties(self):
         return np.sum(self._states) == self._matrix_size
 
-    def run(self, num_steps, initial_state, dec_val=1):
+    def run(self, num_steps, initial_state, dec_val=1, sim_callback=None):
         if initial_state < 0 or initial_state >= len(f):
             return -1
-        assert (
-            self._check_chain_properties()
-        ), "Chain does not have probabilities summing to 1"
         current_state = initial_state
         # Need this to accurately update current state
         state_range = np.arange(self._matrix_size)
         while num_steps > 0:
+            assert (
+                self._check_chain_properties()
+            ), "Chain does not have probabilities summing to 1"
             current_state = np.random.choice(state_range, p=self._states[current_state])
             num_steps -= dec_val
+            if sim_callback is not None:
+                sim_callback(self, current_state, num_steps)
         return current_state
